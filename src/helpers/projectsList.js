fc4128df2b0c28abe3b736de40181c5c39fc9926
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-11 00:34:39 +0000

    feat: add new project weather app

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,86 +1,106 @@
-import prj1 from "assets/img/projects/01.png";
-import prj1Big from "assets/img/projects/01.png";
+import prj1 from 'assets/img/projects/01.png'
+import prj1Big from 'assets/img/projects/01.png'
 
-import prj2 from "assets/img/projects/02.png";
-import prj2Big from "assets/img/projects/02.png";
+import prj2 from 'assets/img/projects/02.png'
+import prj2Big from 'assets/img/projects/02.png'
 
-import prj3 from "assets/img/projects/03.png";
-import prj3Big from "assets/img/projects/03.png";
+import prj3 from 'assets/img/projects/03.png'
+import prj3Big from 'assets/img/projects/03.png'
 
-import prj4 from "assets/img/projects/04.png";
-import prj4Big from "assets/img/projects/04.png";
+import prj4 from 'assets/img/projects/04.png'
+import prj4Big from 'assets/img/projects/04.png'
 
-import prj5 from "assets/img/projects/05.png";
-import prj5Big from "assets/img/projects/05.png";
+import prj5 from 'assets/img/projects/05.png'
+import prj5Big from 'assets/img/projects/05.png'
 
-import prj6 from "assets/img/projects/06.png";
-import prj6Big from "assets/img/projects/06.png";
+import prj6 from 'assets/img/projects/06.png'
+import prj6Big from 'assets/img/projects/06.png'
+
+import prj7 from 'assets/img/projects/07.png'
+import prj7Big from 'assets/img/projects/07.png'
 
 export const projectsList = [
-    {
-        id: "1",
-        title: "Robo School",
-        skills: "HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop",
-        img: prj1,
-        imgBig: prj1Big,
-        gitHubLink: "https://github.com/NazarTymiv/robot-school",
-        linkWebsite: "https://robot-school-5a061.web.app/",
-        description:
-            "This is the website of IT courses for programming robots. I was doing this project for improve my skills of HTML & CSS, JavaScript also I did small server using node.js.",
-    },
-    {
-        id: "2",
-        title: "MNTN",
-        skills: "HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop",
-        img: prj2,
-        imgBig: prj2Big,
-        gitHubLink: "https://github.com/NazarTymiv/mntn",
-        linkWebsite: "https://mntn-47ca3.web.app/",
-        description:
-            "This is information website about mountains and hiking guide which I was doing for practice with gulp js and improve my skills with HTML & CSS and JavaScript.",
-    },
-    {
-        id: "3",
-        title: "MOZI",
-        skills: "HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop",
-        img: prj3,
-        imgBig: prj3Big,
-        gitHubLink: "https://github.com/NazarTymiv/mozi",
-        linkWebsite: "https://mozi-fb706.web.app/",
-        description:
-            "Here is you can see bit information website of company which is building products to simplify work for businesses. It was order of my client from London.",
-    },
-    {
-        id: "4",
-        title: "ECOMVISION",
-        skills: "HTML & CSS, Java Script, Node JS, Express JS, Figma, Adobe Photoshop, React, MongoDB, Mui Material, Mui Icons, Nino",
-        img: prj4,
-        imgBig: prj4Big,
-        gitHubLink: "https://github.com/NazarTymiv/fullstack-admin",
-        linkWebsite: "https://admin-frontend-ngk6.onrender.com/",
-        description:
-            "This is very useful admin dashboard where I putted all needed information about products, sales, income etc. I created this project for practice with React, Mui Material and also for improve my skills for working with database MongoDB and creating servers using node.js.",
-    },
-    {
-        id: "5",
-        title: "REACT PIZZA",
-        skills: "HTML & CSS, SCSS, JSX, TSX, TypeScript, Java Script, Node JS, Express JS, Figma, Adobe Photoshop, React JS, MongoDB, Redux Toolkit, Redux, Ajax",
-        img: prj5,
-        imgBig: prj5Big,
-        gitHubLink: "https://github.com/NazarTymiv/react-pizzeria-advanced",
-        linkWebsite: "https://react-pizzeria-advanced.onrender.com",
-        description:
-            "This is online store of pizzas. I was creating this project for show my skills of React, TypeScript, node.js etc. I tried to make full functionality online store of pizzas where you can see all available pizzas, you can add pizza in your cart and after that see your cart with all added pizzas. Also in this project I was doing a lot additional functionality including pagination, searchbar, filters, sorts etc. ",
-    },
-    {
-        id: "6",
-        title: "SPS Chat",
-        skills: "HTML & CSS, JSX, Java Script, Node JS, Express JS, Figma, Adobe Photoshop, React JS, Socket.IO",
-        img: prj6,
-        imgBig: prj6Big,
-        gitHubLink: "#",
-        linkWebsite: "https://sps-chat.onrender.com/",
-        description:
-            "This is very beautiful designed by me anonymous web chat. I created this chat for chatting with my friends. In this chat when you first open it you can see login window you don't need to create any accounts here, just type any username and any name of room. So when you typed name of room and clicked next it will open new room for you with name which you typed another user can type the same name of room and he will be connected to your room and you can chatting with him there. This chat is anonymous because you can put there any name you want and it will not require another personal information. After that when you will join to any room you will not see messages there because all previous messages are invisible for new users and every time when you will reload page all messages will be removed for you.",
-    },
-];
+  {
+    id: '1',
+    title: 'Robo School',
+    skills:
+      'HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop',
+    img: prj1,
+    imgBig: prj1Big,
+    gitHubLink: 'https://github.com/NazarTymiv/robot-school',
+    linkWebsite: 'https://robot-school-5a061.web.app/',
+    description:
+      'This is the website of IT courses for programming robots. I was doing this project for improve my skills of HTML & CSS, JavaScript also I did small server using node.js.'
+  },
+  {
+    id: '2',
+    title: 'MNTN',
+    skills:
+      'HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop',
+    img: prj2,
+    imgBig: prj2Big,
+    gitHubLink: 'https://github.com/NazarTymiv/mntn',
+    linkWebsite: 'https://mntn-47ca3.web.app/',
+    description:
+      'This is information website about mountains and hiking guide which I was doing for practice with gulp js and improve my skills with HTML & CSS and JavaScript.'
+  },
+  {
+    id: '3',
+    title: 'MOZI',
+    skills:
+      'HTML & CSS, Java Script, Gulp JS, Node JS, Express JS, Figma, Adobe Photoshop',
+    img: prj3,
+    imgBig: prj3Big,
+    gitHubLink: 'https://github.com/NazarTymiv/mozi',
+    linkWebsite: 'https://mozi-fb706.web.app/',
+    description:
+      'Here is you can see bit information website of company which is building products to simplify work for businesses. It was order of my client from London.'
+  },
+  {
+    id: '4',
+    title: 'ECOMVISION',
+    skills:
+      'HTML & CSS, Java Script, Node JS, Express JS, Figma, Adobe Photoshop, React, MongoDB, Mui Material, Mui Icons, Nino',
+    img: prj4,
+    imgBig: prj4Big,
+    gitHubLink: 'https://github.com/NazarTymiv/fullstack-admin',
+    linkWebsite: 'https://admin-frontend-ngk6.onrender.com/',
+    description:
+      'This is very useful admin dashboard where I putted all needed information about products, sales, income etc. I created this project for practice with React, Mui Material and also for improve my skills for working with database MongoDB and creating servers using node.js.'
+  },
+  {
+    id: '5',
+    title: 'REACT PIZZA',
+    skills:
+      'HTML & CSS, SCSS, JSX, TSX, TypeScript, Java Script, Node JS, Express JS, Figma, Adobe Photoshop, React JS, MongoDB, Redux Toolkit, Redux, Ajax',
+    img: prj5,
+    imgBig: prj5Big,
+    gitHubLink: 'https://github.com/NazarTymiv/react-pizzeria-advanced',
+    linkWebsite: 'https://react-pizzeria-advanced.onrender.com',
+    description:
+      'This is online store of pizzas. I was creating this project for show my skills of React, TypeScript, node.js etc. I tried to make full functionality online store of pizzas where you can see all available pizzas, you can add pizza in your cart and after that see your cart with all added pizzas. Also in this project I was doing a lot additional functionality including pagination, searchbar, filters, sorts etc. '
+  },
+  {
+    id: '6',
+    title: 'SPS Chat',
+    skills:
+      'HTML & CSS, JSX, Node JS, Express JS, Figma, Adobe Photoshop, React JS, Socket.IO',
+    img: prj6,
+    imgBig: prj6Big,
+    gitHubLink: 'https://github.com/NazarTymiv/SPS',
+    linkWebsite: 'https://sps-chat.onrender.com/',
+    description:
+      "This is very beautiful designed by me anonymous web chat. I created this chat for chatting with my friends. In this chat when you first open it you can see login window you don't need to create any accounts here, just type any username and any name of room. So when you typed name of room and clicked next it will open new room for you with name which you typed another user can type the same name of room and he will be connected to your room and you can chatting with him there. This chat is anonymous because you can put there any name you want and it will not require another personal information. After that when you will join to any room you will not see messages there because all previous messages are invisible for new users and every time when you will reload page all messages will be removed for you."
+  },
+  {
+    id: '7',
+    title: 'Weather App',
+    skills: 'HTML & CSS, JSX, Figma, Adobe Photoshop, React JS, RestAPI',
+    img: prj7,
+    imgBig: prj7Big,
+    gitHubLink: 'https://github.com/NazarTymiv/weather-app',
+    linkWebsite: 'https://weather-app-nazar.netlify.app/',
+    description:
+      'During my time at Boolean, I developed a comprehensive weather application. This app utilizes geolocation services to pinpoint your current location, sends a request to Api, and seamlessly presents the current weather conditions in your region.'
+  }
+]
