177221925a4c840f534616d4c0decf6665d81399
Nazar Tymiv <nazartumiv@gmail.com>
2024-03-22 02:18:55 +0000

    feat: add typing animation to header text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,25 +1,34 @@
-import React from "react";
-import "./style.css";
+import React from 'react'
+import './style.css'
 
-import CV from "assets/MyCV.pdf";
+import CV from 'assets/MyCV.pdf'
+import { ReactTyped } from 'react-typed'
 
 export const Header = () => {
-    return (
-        <header className="header">
-            <div className="header__wrapper">
-                <h1 className="header__title">
-                    <strong>
-                        Hi, my name is <em>Nazar</em>
-                    </strong>
-                    <br />a Software Engineer
-                </h1>
-                <div className="header__text">
-                    <p>with passion for learning and creating.</p>
-                </div>
-                <a href={CV} download="MyCV.pdf" className="btn">
-                    Download CV
-                </a>
-            </div>
-        </header>
-    );
-};
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__title">
+          <ReactTyped
+            strings={[
+              '<strong>Hi, my name is <em>Nazar</em></strong><br />a Front end',
+              '<strong>Hi, my name is <em>Nazar</em></strong><br />a Back end',
+              '<strong>Hi, my name is <em>Nazar</em></strong><br />a Software Engineer'
+            ]}
+            startDelay={200}
+            backDelay={500}
+            typeSpeed={80}
+            backSpeed={50}
+            onComplete={(self) => (self.cursor.style.opacity = 0)}
+          />
+        </h1>
+        <div className="header__text">
+          <p>with passion for learning and creating.</p>
+        </div>
+        <a href={CV} download="MyCV.pdf" className="btn">
+          Download CV
+        </a>
+      </div>
+    </header>
+  )
+}
